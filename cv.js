9d6db3474b1ce0960d01e90b310a3d24285e6f54
RICHIE-RUARA <gichangaruaraict@gmail.com>
2025-03-22 18:42:52 +0300

    first commit

---
+++ cv.js
@@ -0,0 +1,29 @@
+//  Navigation
+const slidebar = document.querySelector('.slidebar');
+const navLinks = document.querySelector('.nav-links');
+
+slidebar.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Closing menu when clicking a link
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 70,
+            behavior: 'smooth'
+        });
+    });
+});
